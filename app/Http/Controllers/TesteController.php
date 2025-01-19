3e3d15135b994f2b41dfdad880ea4ea1b60f52b7
LeSilveira <le.silveira@yahoo.com.br>
2025-01-19 15:17:42 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function teste(){
+        // $teste = 1;
+        // $teste2 = 2;
+        // $teste3 = 3;
+        // $teste4 = 4;
+        // $teste5 = 5;
+        // print_r(compact(['teste', 'teste2'], ['teste3', 'teste4'], ['teste5'])['teste2']);
+        return 'teste';
+    }
+}
